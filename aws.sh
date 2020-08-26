b707c0577bf8ff3a14dc04763a750630679e2220
Kenchesh Navaluti <67456407+kenchesh01@users.noreply.github.com>
2020-08-26 16:13:30 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,1 +1,99 @@
+ssh ubuntu@$ip '
+sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDhj3oGhTRtgz/nCmIX1IZPCfnoR/nbLfzrDMTgun+o/T9xUM3bUdqyLpKAn25NSvYrpoYkOzu51rOmUb1HGgd7TNEpAoVixskRiNeOv3iE1GDe3v0MuESGeeQuty9vNyLLVVrad56ZfNzyHdUoXBx1rkS6jFfbLeiAcDiBGojHsPsNoP7NNT4PYZaeLysvwdv7LWAUifR4fm5oC2O5ivk4E1YjZeCsDkFSQcCpI2YwFkAYCKMeY9xU+ll9SuMp8bMLo+GVMNeIFUlKzfMAqc/1255cgUPz+tDaF32Nl9PoiVJ5LhEGUyFXIsLH9qfxMHuVkXnapoQMzIQdD2+d9HZZ jenkins@jenkins-01
+" >> /home/ubuntu/.ssh/authorized_keys 
+sudo apt-get update
+sudo apt-get install nagios-nrpe-server nagios-plugins -y
+sudo chmod 777 -R /etc/nagios/
+sudo echo " allowed_hosts=127.0.0.1,172.31.19.37" >> /etc/nagios/nrpe.cfg
+sudo service nagios-nrpe-server restart '
 
+
+
+ssh ubuntu@172.31.19.37 '
+sudo chmod 777 -R /usr/local/nagios/
+sudo echo " 
+define host {
+        use                          linux-server
+        host_name                    nagios-slave
+        alias                        Ubuntu Host
+        address                     172.31.27.223
+        register                     1
+}
+define host {
+        use                          linux-server
+        host_name                    nagios-slave-2
+        alias                        Ubuntu Host
+        address                       $ip
+        register                     1
+}
+define service{
+     use                     generic-service
+     host_name               nagios-slave-2,nagios-slave-2
+     service_description     check-host-alive
+      check_command          check-host-alive
+}
+
+define service {
+      host_name                       nagios-slave,nagios-slave-2
+      service_description             PING
+      check_command                   check_ping!100.0,20%!500.0,60%
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+define service {
+      host_name                       nagios-slave
+      service_description             Check SSH
+      check_command                   check_ssh
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+define service {
+      host_name                       nagios-slave
+      service_description             Total Process
+      check_command                   check_local_procs!250!400!RSZDT
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       nagios-slave
+      service_description             Local Disk
+      check_command                   check_local_disk!20%!10%!/
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+" >> /usr/local/nagios/etc/services/slave.cfg
+sudo systemctl restart nagios
+'
