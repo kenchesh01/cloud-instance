2e99638bdd5d397d4261fe1d74d3c07cf6886004
kenchesh01 <navaluti1997@gmail.com>
2020-08-31 18:08:35 +0530

    kkk

--- instance.sh
+++ instance.sh
@@ -1,8 +1,8 @@
 aws ec2 run-instances \
-	--image-id ami-0bbe28eb2173f6167 \
+	--image-id ami-0bbe28eb2173f6167\
 	--instance-type t2.micro \
 	--subnet-id subnet-6fad9515 \
-	--security-group-ids sg-0c1c2639e1545ee38 \
+	--security-group-ids  sg-0c1c2639e1545ee38 \
 	--associate-public-ip-address \
 	--key-name jenkins \
 	--region us-east-2
