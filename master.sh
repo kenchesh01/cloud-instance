5af3ad5390d8468c8c600bc0780ec7f2cdb9b475
Kenchesh Navaluti <67456407+kenchesh01@users.noreply.github.com>
2020-08-31 10:50:40 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -3,34 +3,19 @@ sudo chmod 777 -R /usr/local/nagios/
 sudo echo " 
 define host {
         use                          linux-server
-        host_name                    nagios-slave5
+        host_name                    nagios-slave2
         alias                        Ubuntu Host
         address                       $ip
         register                     1
 }
 define service{
      use                     generic-service
-     host_name               nagios-slave5
+     host_name               nagios-slave2
      service_description     check-host-alive
       check_command          check-host-alive
 }
 define service {
-      host_name                       nagios-slave5
-      service_description             PING
-      check_command                   check_ping!100.0,20%!500.0,60%
-      max_check_attempts              2
-      check_interval                  2
-      retry_interval                  2
-      check_period                    24x7
-      check_freshness                 1
-      contact_groups                  admins
-      notification_interval           2
-      notification_period             24x7
-      notifications_enabled           1
-      register                        1
-}
-define service {
-      host_name                       nagios-slave5
+      host_name                       nagios-slave2
       service_description             Check SSH
       check_command                   check_ssh
       max_check_attempts              2
@@ -45,7 +30,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       nagios-slave5
+      host_name                       nagios-slave2
       service_description             Total Process
       check_command                   check_local_procs!250!400!RSZDT
       max_check_attempts              2
@@ -60,7 +45,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       nagios-slave5
+      host_name                       nagios-slave2
       service_description             Local Disk
       check_command                   check_local_disk!20%!10%!/
       max_check_attempts              2
@@ -74,6 +59,6 @@ define service {
       notifications_enabled           1
       register                        1
 }
-" >> /usr/local/nagios/etc/services/slave5.cfg
+" >> /usr/local/nagios/etc/services/slave2.cfg
 sudo systemctl restart nagios
 '
