c0b5da556e08232202eb89709e8b6494a3ab28ec
kenchesh01 <navaluti1997@gmail.com>
2020-09-02 11:25:04 +0530

    ss

--- auto-instance.sh
+++ auto-instance.sh
@@ -5,7 +5,7 @@ aws ec2 run-instances \
 	--security-group-ids sg-0c1c2639e1545ee38 \
 	--associate-public-ip-address \
 	--key-name jenkins \
-        --region us-east-2
+    --region us-east-2
     
     
 ip=`aws ec2 describe-instances --region us-east-2 --query 'sort_by(Reservations[].Instances[], &LaunchTime)[].[InstanceId,PublicIpAddress,LaunchTime]' --output text | tail -1 | awk '{ print $2 }'`
@@ -14,7 +14,7 @@ sudo apt-get install openssh-client openssh-server
 sudo ufw allow 22
 sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDhj3oGhTRtgz/nCmIX1IZPCfnoR/nbLfzrDMTgun+o/T9xUM3bUdqyLpKAn25NSvYrpoYkOzu51rOmUb1HGgd7TNEpAoVixskRiNeOv3iE1GDe3v0MuESGeeQuty9vNyLLVVrad56ZfNzyHdUoXBx1rkS6jFfbLeiAcDiBGojHsPsNoP7NNT4PYZaeLysvwdv7LWAUifR4fm5oC2O5ivk4E1YjZeCsDkFSQcCpI2YwFkAYCKMeY9xU+ll9SuMp8bMLo+GVMNeIFUlKzfMAqc/1255cgUPz+tDaF32Nl9PoiVJ5LhEGUyFXIsLH9qfxMHuVkXnapoQMzIQdD2+d9HZZ jenkins@jenkins-01
 " >> /home/ubuntu/.ssh/authorized_keys 
-sudo hostname Nagios-slave02
+sudo hostname Nagios-slave3
 sudo apt-get update
 sudo apt-get install nagios-nrpe-server nagios-plugins -y
 sudo chmod 777 -R /etc/nagios/
@@ -29,14 +29,14 @@ sudo chmod 777 -R /usr/local/nagios/
 sudo echo " 
 define host {
         use                          linux-server
-        host_name                    nagios-slave02
+        host_name                    nagios-slave3
         alias                        Ubuntu Host
         address                       '$ip'
         register                     1
 }
 define service {
        use                            generic-service
-      host_name                       nagios-slave02
+      host_name                       nagios-slave3
       service_description             Check SSH
       check_command                   check_ssh
       max_check_attempts              2
@@ -51,7 +51,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       nagios-slave02
+      host_name                       nagios-slave3
       service_description             Total Process
       check_command                   check_local_procs!250!400!RSZDT
       max_check_attempts              2
@@ -66,7 +66,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       nagios-slave02
+      host_name                       nagios-slave3
       service_description             Local Disk
       check_command                   check_local_disk!20%!10%!/
       max_check_attempts              2
@@ -81,7 +81,7 @@ define service {
       register                        1
 }
 define service {
-host_name                   nagios-slave02
+host_name                   nagios-slave3
 service_description        Check Users
 check_command              check_local_users!20!50
 max_check_attempts           2
@@ -95,6 +95,6 @@ notification_period             24x7
 notifications_enabled           1
 register                        1
 }
-" >> /usr/local/nagios/etc/services/slave2.cfg
+" >> /usr/local/nagios/etc/services/slave3.cfg
 sudo systemctl restart nagios
 '
